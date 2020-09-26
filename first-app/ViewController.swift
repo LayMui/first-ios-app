5b0508a69f82af5d634c23eed1497a792c453681
Lay Mui <mui_ume@hotmail.com>
2020-09-26 20:56:40 +0800

    Add files via upload

---
+++ first-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first-app
+//
+//  Created by Lay mui Toh on 26/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
